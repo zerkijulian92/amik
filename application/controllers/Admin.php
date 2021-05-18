269fb3aa7a2a6e34c9e90945d526b7a6ace99222
Zerky Yulyandri <zerkijuliandri92@gmail.com>
2021-05-18 10:09:21 +0700

    Edit file header.php, sidebar.php, footer.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -20,6 +20,9 @@ class Admin extends CI_Controller {
 	 */
 	public function dashboard()
 	{
+		$this->load->view('templates/header');
+		$this->load->view('templates/sidebar');
 		$this->load->view('dashboard');
+		$this->load->view('templates/footer');
 	}
 }
