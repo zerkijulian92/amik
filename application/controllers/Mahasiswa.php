318c136c03746a02a6ac1236f1c006f0e6ba321d
Zerky Yulyandri <zerkijuliandri92@gmail.com>
2021-05-18 11:18:00 +0700

    Membuat Function tampil_data() di controllers mahasiswa.php

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -4,5 +4,9 @@ class Mahasiswa extends CI_Controller{
 	public function index()
 	{
 		$data['mahasiswa'] =$this->m_mahasiswa->tampil_data()->result();
+		$this->load->view('templates/header');
+		$this->load->view('templates/sidebar');
+		$this->load->view('mahasiswa', $data);
+		$this->load->view('templates/footer');
 	}
 }
